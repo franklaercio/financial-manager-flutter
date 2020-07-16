4f9bab4335b2f0be5b343dfffc61d39f323061f9
franklaercio <franklaercio@gmail.com>
2020-07-15 23:48:24 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,11 +16,19 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter App'),),
-      body: Column(children: <Widget>[
-        Container(width: double.infinity, child: Card(child: Text('Chart!'),elevation: 5)),
-        Card(child: Text('LIST OF TX!'),),
-      ],),
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              child: Card(child: Text('Chart!'), elevation: 5)),
+          Card(
+            child: Text('LIST OF TX!'),
+          ),
+        ],
+      ),
     );
   }
 }
