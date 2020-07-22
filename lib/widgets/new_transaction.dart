caac9825c1a3c75b4e452b6e34f7f15bc589648e
franklaercio <franklaercio@gmail.com>
2020-07-21 21:51:18 -0300

    Respecting the softkeyboard Insets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -55,54 +55,61 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              keyboardType: TextInputType.text,
-              onSubmitted: (_) => _submitData,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData,
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    _selectedDate == null
-                        ? 'No Date Chosen!'
-                        : 'Picked Date: ' +
-                            DateFormat.yMd().format(_selectedDate),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                keyboardType: TextInputType.text,
+                onSubmitted: (_) => _submitData,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ' +
+                              DateFormat.yMd().format(_selectedDate),
+                    ),
                   ),
-                ),
-                FlatButton(
-                  onPressed: _presentDatePicker,
-                  child: Text(
-                    'Choose Date',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor),
+                  FlatButton(
+                    onPressed: _presentDatePicker,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                    textColor: Theme.of(context).primaryColor,
                   ),
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
-            ),
-            RaisedButton(
-              child: Text('Add Transactions'),
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              onPressed: _submitData,
-            )
-          ],
+                ],
+              ),
+              RaisedButton(
+                child: Text('Add Transactions'),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                onPressed: _submitData,
+              )
+            ],
+          ),
         ),
       ),
     );
